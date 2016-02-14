c540b1ab43e78e8d74607a314426a47b76c37615
Erik Ostrom <erik@echographia.com>
2016-02-14 16:56:52 -0600

    Create Set API.

--- app/controllers/exercise_sets_controller.rb
+++ app/controllers/exercise_sets_controller.rb
@@ -1,2 +1,13 @@
 class ExerciseSetsController < ApplicationController
+  def create
+    @set = ExerciseSet.create(set_params)
+
+    render json: @set
+  end
+
+  private
+
+  def set_params
+    params.require(:exercise_set).permit(:exercise_name, :reps, :weight)
+  end
 end
--- app/models/exercise_set.rb
+++ app/models/exercise_set.rb
@@ -2,6 +2,13 @@ class ExerciseSet < ApplicationRecord
   belongs_to :workout
   belongs_to :exercise
 
-  validates :position, :reps, :weight,
+  validates :reps, :weight,
     numericality: {greater_than: 0}
+
+  validates :exercise,
+    presence: true
+
+  def exercise_name=(name)
+    self.exercise = Exercise.find_or_create_by(name: name)
+  end
 end
