3e69d929044d796600720732a7d59b0196a0786e
Erik Ostrom <erik@echographia.com>
2016-02-16 21:18:19 -0600

    Ignore Workouts for now.
    But make sure all exercises have names.

--- app/models/exercise_set.rb
+++ app/models/exercise_set.rb
@@ -1,6 +1,8 @@
 class ExerciseSet < ApplicationRecord
-  belongs_to :workout
   belongs_to :exercise
+  # We're not using Workouts yet, so I'm not going to bother creating
+  # them. If we need them later, we can reconstruct them from the data.
+  belongs_to :workout, optional: true
 
   validates :reps, :weight,
     numericality: {greater_than: 0}
@@ -9,6 +11,6 @@ class ExerciseSet < ApplicationRecord
     presence: true
 
   def exercise_name=(name)
-    self.exercise = Exercise.find_or_create_by(name: name)
+    self.exercise = Exercise.find_or_create_by(name: name) if name.present?
   end
 end
